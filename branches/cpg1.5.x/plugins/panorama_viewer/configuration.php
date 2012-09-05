6bc2a0282ca73e5930bbdf96cf583f5dc254b4a3
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2012-09-05 13:01:30 +0000

    Added configuration interface.

--- branches/cpg1.5.x/plugins/panorama_viewer/configuration.php
+++ branches/cpg1.5.x/plugins/panorama_viewer/configuration.php
@@ -15,11 +15,12 @@
   $Date$
   **************************************************/
 
+global $lang_gallery_admin_menu;
 $name = 'Panorama Viewer';
 $description = 'Simple panorama image viewer';
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
-$version = '1.3';
+$version = '1.4';
 $plugin_cpg_version = array('min' => '1.5');
 $extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,59986.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).'Announcement thread</a>';
-
+$extra_info = '<a href="index.php?file=panorama_viewer/admin" class="admin_menu">'.cpg_fetch_icon('config', 1).$name.' '.$lang_gallery_admin_menu['admin_lnk'].'</a>'.$extra_info;
 ?>
