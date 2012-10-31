bbafe5e2f06b67aaf6b4f4045097ac2ddbccd89e
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2012-10-31 15:32:36 +0000

    Added limit_page_views plugin.

---
+++ branches/cpg1.5.x/plugins/limit_page_views/configuration.php
@@ -0,0 +1,24 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - limit_page_views
+  *************************************************
+  Copyright (c) 2012 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
+
+$name = 'Limit page views';
+$description = 'If there are more than x page views in a specific time, stop the processing and display a message instead.';
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
+$version = '0.1';
+$plugin_cpg_version = array('min' => '1.5');
+
+?>
