0350e3fe26357501008b6dab52f28ca42ccdab9b
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-04-18 11:26:57 +0000

    Added Polish language file (user contribution).

---
+++ branches/cpg1.5.x/plugins/cookie_consent/lang/polish.php
@@ -0,0 +1,30 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Cookie consent
+  *************************************************
+  Copyright (c) 2012 eenemeenemuu
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_cookie_consent['name'] = 'Informacja o Ciasteczklach';
+$lang_plugin_cookie_consent['description'] = 'Ten serwis wykorzystuje ciasteczka zgodnie z dyrektywą  UE Cookie %s';
+$lang_plugin_cookie_consent['announcement_thread'] = 'Announcement thread';
+$lang_plugin_cookie_consent['why_cookies'] = 'Ta strona umieszcza ciasteczka na twoim komputerze. <a href="index.php?file=cookie_consent/info">Dowiedz się więcej</a>.';
+$lang_plugin_cookie_consent['accept'] = 'Akceptuję ciasteczka z tej strony internetowej.';
+$lang_plugin_cookie_consent['cookie_name'] = 'Oznaczenie Ciasteczek';
+$lang_plugin_cookie_consent['used_for'] = 'Wykorzytane do';
+$lang_plugin_cookie_consent['cookie_data'] = 'Ustawień użytkownika (np. język, porządek sortowania, parametry wyszukiwania)';
+$lang_plugin_cookie_consent['cookie_cpg_session'] = 'Ciasteczka sesji niezintegrowanej galerii (niepowiązanej z innymi aplikacjami)';
+$lang_plugin_cookie_consent['cookie_mambo_sessioncookie'] = 'Ciasteczka sesji zintegrowanej galerii (powiązanej z innymi aplikacjami)';
+$lang_plugin_cookie_consent['cookie_picinfo'] = 'Przechowuje ostatni stan wyświetlania lub ukrywania w oknie informacji o pliku';
+$lang_plugin_cookie_consent['cookie_fav'] = 'Ulubione zdjęcia';
+$lang_plugin_cookie_consent['cookie_albpw'] = 'Zapamiętywania haseł do albumu';
+$lang_plugin_cookie_consent['cookie_client_id'] = '"32&nbsp;digit&nbsp;string"';
+
+?>
