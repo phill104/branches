5eb61f9092ce9d9fd5803e873d8e788d26b59b42
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-04-18 07:27:42 +0000

    Updated URL to plugin announcement thread.

--- branches/cpg1.5.x/plugins/cookie_consent/configuration.php
+++ branches/cpg1.5.x/plugins/cookie_consent/configuration.php
@@ -18,10 +18,10 @@ if ($CONFIG['lang'] != 'english' && file_exists("./plugins/cookie_consent/lang/{
 }
 
 $name = $lang_plugin_cookie_consent['name'];
-$description = sprintf($lang_plugin_cookie_consent['description'], '<a href="http://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CELEX:32009L0136:EN:NOT" rel="external" class="external">Directive 2009/136/EC</a>');
+$description = sprintf($lang_plugin_cookie_consent['description'], '<a href="http://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CELEX:32009L0136:EN:NOT" rel="external" class="external">2009/136/EC</a>');
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
 $version = '1.0';
 $plugin_cpg_version = array('min' => '1.5.22');
-$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_cookie_consent['announcement_thread'].'</a>';
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,76197.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_cookie_consent['announcement_thread'].'</a>';
 
 ?>
