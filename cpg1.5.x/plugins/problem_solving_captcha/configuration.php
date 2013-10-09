68a05262f1c4f9b060409e06cafc6b8aaa342f14
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-10-09 08:32:56 +0000

    Updated announcement thread URL.

--- cpg1.5.x/plugins/problem_solving_captcha/configuration.php
+++ cpg1.5.x/plugins/problem_solving_captcha/configuration.php
@@ -26,6 +26,6 @@ $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile
 $version = '1.0';
 $plugin_cpg_version = array('min' => '1.5');
 $extra_info = '<a href="index.php?file=problem_solving_captcha/admin" class="admin_menu">'.cpg_fetch_icon('config', 1).$name.' '.$lang_gallery_admin_menu['admin_lnk'].'</a>';
-$extra_info .= $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_problem_solving_captcha['announcement_thread'].'</a>';
+$extra_info .= $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,76723.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_problem_solving_captcha['announcement_thread'].'</a>';
 
 ?>
