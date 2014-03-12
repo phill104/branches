ef02f8cc8f3ad25360e94e5ba88490438238e83d
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2014-03-12 12:34:52 +0000

    Added 'hidden_features' plugin.

---
+++ cpg1.5.x/plugins/hidden_features/configuration.php
@@ -0,0 +1,33 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x plugin - hidden_features
+  *************************************************
+  Copyright (c) 2014 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+require "./plugins/hidden_features/lang/english.php";
+if ($CONFIG['lang'] != 'english' && file_exists("./plugins/hidden_features/lang/{$CONFIG['lang']}.php")) {
+    require "./plugins/hidden_features/lang/{$CONFIG['lang']}.php";
+}
+
+$name = $lang_plugin_hidden_features['hidden_features'];
+$description = $lang_plugin_hidden_features['description'];
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
+$version = '1.0';
+$plugin_cpg_version = array('min' => '1.5');
+
+
+$install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_hidden_features['announcement_thread'].'</a>';
+$extra_info = '<a href="index.php?file=hidden_features/admin" class="admin_menu">'.cpg_fetch_icon('config', 1).$name.' '.$lang_gallery_admin_menu['admin_lnk'].'</a>'.$install_info;
+
+?>
---
+++ cpg1.5.x/plugins/hidden_features/lang/english.php
@@ -0,0 +1,31 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x plugin - hidden_features
+  *************************************************
+  Copyright (c) 2014 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_hidden_features['hidden_features'] = 'Hidden features';
+$lang_plugin_hidden_features['description'] = 'Adds a config panel for all available hidden features.';
+$lang_plugin_hidden_features['announcement_thread'] = 'Announcement thread';
+$lang_plugin_hidden_features['saved'] = 'Your settings have been saved.';
+$lang_plugin_hidden_features['allow_guests_enter_file_details'] = 'Allow guests to enter file details after flash upload';
+$lang_plugin_hidden_features['upload_create_album_directory'] = 'Create sub-directory for each album in users\' upload directories';
+$lang_plugin_hidden_features['album_sort_order'] = 'Sort order for albums';
+$lang_plugin_hidden_features['custom_sortorder_thumbs'] = 'Display sort buttons on thumbnail page';
+$lang_plugin_hidden_features['editpics_ignore_newer_than'] = 'Show previously uploaded files at edit files form after flash upload';
+$lang_plugin_hidden_features['link_last_upload'] = 'Regard upload time of linked files in album info';
+
+?>
