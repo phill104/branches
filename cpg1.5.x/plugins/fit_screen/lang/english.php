97349209f4807368e161d6c9d501b75ed12ab8bb
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2014-03-27 14:19:22 +0000

    Added 'fit_screen' plugin.

---
+++ cpg1.5.x/plugins/fit_screen/lang/english.php
@@ -0,0 +1,24 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x plugin - fit_screen
+  *************************************************
+  Copyright (c) 2014 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_fit_screen['fit_screen'] = 'Fit screen';
+$lang_plugin_fit_screen['description'] = 'Use full-sized pictures on the intermediate-sized pictures pages and downscale them if needed to fit the users screen resolution. A click on the picture scales it to full size.';
+$lang_plugin_fit_screen['announcement_thread'] = 'Announcement thread';
+
+?>
