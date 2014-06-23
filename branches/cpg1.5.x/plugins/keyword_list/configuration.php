751202a04264e2e294918a37b8a6654ca8440b5d
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2014-06-23 10:28:30 +0000

    Added cpg1.5.x compatible keyword_list plugin.

---
+++ branches/cpg1.5.x/plugins/keyword_list/configuration.php
@@ -0,0 +1,25 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2006 Coppermine Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.9
+  $Source$
+  $Revision$
+  $Author$
+  $Date$
+**********************************************/
+
+$name='Keyword List';
+$description='Add a menu link to a keyword list page with alphabetical ordering.<br>Two ways to display pic list: by keyword initial or fo each keyword<br/> display a link to each photo with preview image popup';
+$author='<a href="http://coppermine-gallery.net/forum/index.php?action=profile;u=9980">Frantz</a> with wirewolf\'s preview css window popup';
+$version='2.1';
+$plugin_cpg_version = array('min' => '1.5');
+?>
