89f1b3f41804f1bb3917092994e0ae290873175f
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2014-10-09 11:13:29 +0000

    Added French language file (user contribution).

---
+++ cpg1.5.x/plugins/theme_switch/lang/french.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Theme switch
+  *************************************************
+  Copyright (c) 2010-2012 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_theme_switch['theme_switch'] = 'Adaptateur de thème';
+$lang_plugin_theme_switch['description'] = 'Adapte le thème pour les terminaux mobiles (e.g. smartphones)';
+$lang_plugin_theme_switch['announcement_thread'] = 'Sujet d\'annonce';
+$lang_plugin_theme_switch['select'] = 'Thème pour les terminaux mobiles';
+$lang_plugin_theme_switch['saved'] = 'Vos paramètres ont été sauvegardés.';
+$lang_plugin_theme_switch['mobile_view'] = 'Vue mobile';
+$lang_plugin_theme_switch['normal_view'] = 'Vue normale';
+
+?>
