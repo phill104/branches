4caef4753e3d437ec52ea3d175494a63d778d559
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2015-01-31 12:56:19 +0000

    Updated header.

--- cpg1.5.x/plugins/remote_videos/codebase.php
+++ cpg1.5.x/plugins/remote_videos/codebase.php
@@ -2,13 +2,8 @@
 /**************************************************
   Coppermine 1.5.x Plugin - remote_videos
   *************************************************
-  Copyright (c) 2009-2010 eenemeenemuu
+  Copyright (c) 2009-2015 eenemeenemuu
   *************************************************
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
-  ********************************************
   $HeadURL$
   $Revision$
   $LastChangedBy$
