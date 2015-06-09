de94a6484afc4811122dca005b09e1a671dd68b4
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2015-06-09 19:58:14 +0000

    Added option "Add button next to album drop-down box to display only empty albums".

--- branches/cpg1.5.x/plugins/hidden_features/codebase.php
+++ branches/cpg1.5.x/plugins/hidden_features/codebase.php
@@ -29,4 +29,24 @@ function hidden_features_page_start() {
     }
 }
 
+
+function hidden_features_only_empty_albums_button() {
+    global $CONFIG, $CPG_PHP_SELF, $lang_plugin_hidden_features;
+    $superCage = Inspekt::makeSuperCage();
+
+    if ($CONFIG['only_empty_albums'] == 1 || ($CONFIG['only_empty_albums'] == 2 && GALLERY_ADMIN_MODE)) {
+        $sep = strpos($superCage->server->getRaw('REQUEST_URI'), '?') ? '&amp;' : '?';
+        if ($superCage->get->keyExists('only_empty_albums')) {
+            $only_empty_albums = '<a href="'.preg_replace('/[\?&]only_empty_albums/', '', $superCage->server->getRaw('REQUEST_URI')).'" class="button">'.$lang_plugin_hidden_features['only_empty_albums_button_all'].'</a>';
+        } else {
+            $only_empty_albums = '<a href="'.$superCage->server->getRaw('REQUEST_URI').$sep.'only_empty_albums" class="button">'.$lang_plugin_hidden_features['only_empty_albums_button_empty'].'</a>';
+        }
+    } else {
+        $only_empty_albums = '';
+    }
+
+    return $only_empty_albums;
+}
+
+
 ?>
