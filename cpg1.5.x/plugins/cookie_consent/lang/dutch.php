ba1ac04f0f3153a5b4d5693e155c29b9e56596cf
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2015-08-20 15:10:16 +0000

    Added Dutch language file (user contribution).

---
+++ cpg1.5.x/plugins/cookie_consent/lang/dutch.php
@@ -0,0 +1,30 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Cookie consent
+  *************************************************
+  Copyright (c) 2012 eenemeenemuu
+  ********************************************
+  $HeadURL: https://svn.code.sf.net/p/coppermine/code/branches/cpg1.5.x/plugins/cookie_consent/lang/dutch.php $
+  $Revision: 8454 $
+  $LastChangedBy: eenemeenemuu $
+  $Date: 2012-06-29 15:45:03 +0200 (Fr, 29 Jun 2012) $
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_cookie_consent['name'] = 'Cookie toestemming';
+$lang_plugin_cookie_consent['description'] = 'Vraagt gebruikers of Coppermine toestemming heeft om cookies op te slaan in overeenstemming met EU Cookie Richtlijn %s';
+$lang_plugin_cookie_consent['announcement_thread'] = 'Aankondiging';
+$lang_plugin_cookie_consent['why_cookies'] = 'We willen graag cookies op uw computer zetten. Zie <a href="index.php?file=cookie_consent/info">hier</a> voor meer informatie.';
+$lang_plugin_cookie_consent['accept'] = 'Ik accepteer cookies van deze website.';
+$lang_plugin_cookie_consent['cookie_name'] = 'Cookie naam';
+$lang_plugin_cookie_consent['used_for'] = 'Gebruikt voor';
+$lang_plugin_cookie_consent['cookie_data'] = 'Opslaan van meerdere gebruikersinstellingen (o.a. taal, sorteervolgorde, zoekparameters)';
+$lang_plugin_cookie_consent['cookie_cpg_session'] = 'Sessie cookie voor niet-gekoppelde galerijen';
+$lang_plugin_cookie_consent['cookie_mambo_sessioncookie'] = 'Sessie cookie wanneer gekoppeld aan Mambo';
+$lang_plugin_cookie_consent['cookie_picinfo'] = 'Slaat de laaatste status op van het tonen of verbergen van het bestands-informatie venster';
+$lang_plugin_cookie_consent['cookie_fav'] = 'Slaat informatie op over de favoriete foto\'s van de gebruiker';
+$lang_plugin_cookie_consent['cookie_albpw'] = 'Slaat het ingevulde wachtwoord op voor albums';
+$lang_plugin_cookie_consent['cookie_client_id'] = '"32&nbsp;digit&nbsp;string"';
+
+?>
